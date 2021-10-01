6a8c5d6f55470c9e8307eab588c8b51aa9c5589f
Fábrica de Software CEET <84735752+SoftwareFactoryCEET@users.noreply.github.com>
2021-10-01 11:52:28 -0500

    BLL Completa

---
+++ xUnit.Practica03/BLL/ServiceOrientation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BLL
+{
+    public class ServiceOrientation
+    {
+            /// <summary>
+            /// Converts values of an enum representing visual directions in an online map to the corresponding cardinal directions
+            /// </summary>
+            /// <param name="direction"></param>
+            /// <returns></returns>
+            /// <exception cref="ArgumentOutOfRangeException"></exception>
+            public Orientation ToOrientation(Direction direction) => direction switch
+            {
+                Direction.Up => Orientation.North,
+                Direction.Right => Orientation.East,
+                Direction.Down => Orientation.South,
+                Direction.Left => Orientation.West,
+                _ => throw new ArgumentOutOfRangeException(nameof(direction), $"Not expected direction value: {direction}"),
+            };
+
+    }
+}
